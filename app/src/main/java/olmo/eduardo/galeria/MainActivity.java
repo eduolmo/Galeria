55ab7ffde8f180b4ed1cd9632d6651561cf9c9f7
20211TIIMI0044 <eduardo.olmosantana@gmail.com>
2023-05-05 11:09:46 -0300

    Parei na hora de criar o File_paths, mas ainda falta comentar tudo

--- app/src/main/java/olmo/eduardo/galeria/MainActivity.java
+++ app/src/main/java/olmo/eduardo/galeria/MainActivity.java
@@ -1,8 +1,13 @@
 package olmo.eduardo.galeria;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //obtendo a toolbar
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        //configurando a toolbar como padrao
+        setSupportActionBar(toolbar);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        //
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_tb, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()) {
+            case R.id.opCamera:
+                dispatchTakePictureIntent();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 }
